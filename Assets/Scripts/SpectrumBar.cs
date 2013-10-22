4dfb1cc604ba3455fbb34830a8c1c0962109f2a6
Takahashi Keijiro <keijiro@Super-MSX.local>
2013-10-22 19:23:17 +0900

    Test project (cloned from unity-audio-spectrum).

---
+++ Assets/Scripts/SpectrumBar.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpectrumBar : MonoBehaviour
+{
+    public int index;
+    public bool useMeanLevel;
+
+    AudioBridge audioBridge;
+
+    void Awake()
+    {
+        audioBridge = FindObjectOfType(typeof(AudioBridge)) as AudioBridge;
+    }
+
+    void Update ()
+    {
+        var levels = useMeanLevel ? audioBridge.MeanLevels : audioBridge.Levels;
+        if (index < levels.Length) {
+            var vs = transform.localScale;
+            vs.y = levels[index] * 0.2f;
+            transform.localScale = vs;
+        }
+    }
+}
---
+++ Assets/Scripts/Visualizer.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Visualizer : MonoBehaviour
+{
+    public GameObject barPrefab;
+    public GUIStyle labelStyle;
+
+    bool useMeanLevel;
+    int barCount;
+
+    void Update ()
+    {
+        var audioBridge = GetComponent<AudioBridge>();
+
+        if (barCount == audioBridge.Levels.Length && !Input.GetMouseButtonDown(0)) {
+            return;
+        }
+
+        // Change the bar type on mouse click.
+        if (Input.GetMouseButtonDown(0)) {
+            useMeanLevel = !useMeanLevel;
+        }
+
+        // Destroy the old bars.
+        foreach (var child in transform) {
+            Destroy ((child as Transform).gameObject);
+        }
+
+        // Change the number of bars.
+        barCount = audioBridge.Levels.Length;
+        var barWidth = 6.0f / barCount;
+        var barScale = new Vector3 (barWidth * 0.9f, 1, 0.75f);
+
+        // Create new bars.
+        for (var i = 0; i < barCount; i++) {
+            var x = 6.0f * i / barCount - 3.0f + barWidth / 2;
+
+            var bar = Instantiate (barPrefab, Vector3.right * x, transform.rotation) as GameObject;
+
+            bar.GetComponent<SpectrumBar> ().index = i;
+            bar.GetComponent<SpectrumBar> ().useMeanLevel = useMeanLevel;
+
+            bar.transform.parent = transform;
+            bar.transform.localScale = barScale;
+        }
+    }
+
+    void OnGUI ()
+    {
+        var text = "Current mode: " + (useMeanLevel ? "Mean Level" : "Realtime") + "\n";
+        text += "Click the screen to change the mode.";
+        GUI.Label (new Rect(0, 0, Screen.width, Screen.height), text, labelStyle);
+    }
+}
